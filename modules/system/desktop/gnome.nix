cc7c8d6c13f4cf0e839f8b14beeb866c043621d6
joaopfusco <joaopedrofusco@gmail.com>
2026-04-07 00:53:46 -0300

    fix: update comments for clarity in gnome configuration

--- modules/system/desktop/gnome.nix
+++ modules/system/desktop/gnome.nix
@@ -12,7 +12,7 @@
   # Enable the Ozone/Wayland backend for better performance and compatibility with modern hardware.
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-  # Dconf Settings
+  # GNOME Tweaks and Extensions for customization
   environment.systemPackages = with pkgs; [
     gnome-tweaks
     gnome-extension-manager
@@ -21,6 +21,16 @@
     gnomeExtensions.dash-to-dock
   ];
 
+  # Reset GNOME settings to default values
+  /*
+  gsettings reset org.gnome.desktop.interface icon-theme
+  gsettings reset org.gnome.desktop.interface gtk-theme
+  gsettings reset org.gnome.desktop.interface font-name
+  gsettings reset org.gnome.desktop.interface monospace-font-name
+  gsettings reset org.gnome.desktop.interface cursor-theme
+  */
+
+  # Dconf Settings
   programs.dconf.enable = true;
   programs.dconf.profiles.user.databases = [{
     settings = {
