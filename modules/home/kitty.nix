36a23ace0f8d29dc813dfcc83fb0f791881e6b03
joaopfusco <joaopedrofusco@gmail.com>
2026-04-14 23:23:59 -0300

    refactor: update kitty configuration with new theme and appearance settings

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,28 +1,46 @@
 { config, lib, pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
   programs.kitty = {
     enable = true;
+    themeFile = "Catppuccin-Mocha";
+    
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 11;
+    };
     
     settings = {
       shell = "${pkgs.zsh}/bin/zsh --login";
 
-      window_padding_width = 10;
-      remember_window_size = "no";
-      initial_window_width = "1100";
-      initial_window_height = "700";
+      # Window and Appearance
+      window_padding_width = 15;
       confirm_os_window_close = 0;
+      background_opacity = "0.95";
+      hide_window_decorations = "yes";
 
-      font_size = "12.0";
-      disable_ligatures = "never";
-
+      # Cursor and Scroll
+      cursor_shape = "beam";
+      cursor_blink_interval = 0;
       scrollback_lines = 10000;
       enable_audio_bell = false;
       update_check_interval = 0;
+
+      # Performance Wayland
+      input_delay = 3;
+      repaint_delay = 10;
+
+      # Ligatures
+      disable_ligatures = "never";
       
-      background_opacity = "1.0";
+      # URL Handling
+      url_color = "#89b4fa";
+      url_style = "curly";
+      open_url_with = "default";
     };
-    
-    themeFile = "Dracula";
   };
 }
