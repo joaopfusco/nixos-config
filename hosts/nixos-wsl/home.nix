cf56f293a234da7e0af03633dc9a7dce2f84322a
joaopfusco <joaopedrofusco@gmail.com>
2026-05-14 23:50:10 -0300

    fix: dotnet nix on wsl

--- hosts/nixos-wsl/home.nix
+++ hosts/nixos-wsl/home.nix
@@ -6,7 +6,7 @@
     ../../modules/home/git.nix
     ../../modules/home/zsh.nix
     ../../modules/home/direnv.nix
-    ../../modules/home/dotnet/base.nix
+    ../../modules/home/dotnet/full.nix
     ../../modules/home/zed
   ];
 
